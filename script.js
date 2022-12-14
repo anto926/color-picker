4867a2a9cc7adefd9d0592d591b0e12e48302335
anto926 <anto_96_asr@live.it>
2022-12-14 12:43:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,25 @@ document.querySelector("#red").addEventListener("click", function(e){
     body.style.backgroundColor = color
 })
 
-document.querySelector("#blue").addEventListener("click", function(e){
+document.querySelector("#lightblue").addEventListener("click", function(e){
+    let color = e.target.id
+
+    body.style.backgroundColor = color
+})
+
+document.querySelector("#lightgreen").addEventListener("click", function(e){
+    let color = e.target.id
+
+    body.style.backgroundColor = color
+})
+
+document.querySelector("#violet").addEventListener("click", function(e){
+    let color = e.target.id
+
+    body.style.backgroundColor = color
+})
+
+document.querySelector("#yellow").addEventListener("click", function(e){
     let color = e.target.id
 
     body.style.backgroundColor = color
