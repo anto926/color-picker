7d609c6f2082577e04ce452a8e3a4e02f3e8b4ff
anto926 <anto_96_asr@live.it>
2022-12-14 12:23:47 +0100

    Aggiunta file

---
+++ script.js
@@ -0,0 +1,17 @@
+document.querySelector("#green").addEventListener("click", function(e){
+    let color = e.target.id
+
+    body.style.backgroundColor = color
+})
+
+document.querySelector("#red").addEventListener("click", function(e){
+    let color = e.target.id
+
+    body.style.backgroundColor = color
+})
+
+document.querySelector("#blue").addEventListener("click", function(e){
+    let color = e.target.id
+
+    body.style.backgroundColor = color
+})
